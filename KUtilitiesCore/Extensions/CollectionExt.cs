64875fe41fd2fa51f068993af561b2ec45d63901
Mike <dark_mabg@hotmail.com>
2025-04-07 11:13:45 -0600

    Se agregaron extensiones y un archivo de recursos de nombre de colores

---
+++ KUtilitiesCore/Extensions/CollectionExt.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KUtilitiesCore.Extensions
+{
+    /// <summary>
+    /// Funcionalidades extendidas para algunas colecciones
+    /// </summary>
+    public static class CollectionExt
+    {
+        /// <summary>
+        /// Ejecuta una acción por cada elemento de la colección.
+        /// </summary>
+        /// <typeparam name="T">El tipo de los elementos de la colección.</typeparam>
+        /// <param name="collection">La colección a iterar.</param>
+        /// <param name="iterationAction">La acción a ejecutar por cada elemento.</param>
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T> iterationAction)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if (iterationAction == null) throw new ArgumentNullException(nameof(iterationAction));
+
+            foreach (T item in collection)
+            {
+                iterationAction(item);
+            }
+        }
+
+        /// <summary>
+        /// Ejecuta una acción por cada elemento de la colección, proporcionando el índice del elemento.
+        /// </summary>
+        /// <typeparam name="T">El tipo de los elementos de la colección.</typeparam>
+        /// <param name="collection">La colección a iterar.</param>
+        /// <param name="iterationAction">La acción a ejecutar por cada elemento, con el índice del elemento.</param>
+        public static void ForEach<T>(this IEnumerable<T> collection, Action<T, int> iterationAction)
+        {
+            if (collection == null) throw new ArgumentNullException(nameof(collection));
+            if (iterationAction == null) throw new ArgumentNullException(nameof(iterationAction));
+
+            int index = 0;
+            foreach (T item in collection)
+            {
+                iterationAction(item, index++);
+            }
+        }
+    }
+}
