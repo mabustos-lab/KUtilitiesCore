4b63c5b3e3bcf4d8d7e6adcbd1aa18fa8a1d071f
Mike <dark_mabg@hotmail.com>
2025-04-13 20:00:04 -0600

    Nuevas extensiones

---
+++ KUtilitiesCore/Data/ValidationAttributesExt.cs
@@ -0,0 +1,61 @@
+﻿using KUtilitiesCore.Extensions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KUtilitiesCore.Data
+{
+    public static class ValidationAttributesExt
+    {
+        /// <summary>
+        /// Valida las propiedades de un objeto usando <see cref="ValidationAttribute"/>.
+        /// </summary>
+        /// <typeparam name="TSource">Tipo del objeto a validar.</typeparam>
+        /// <param name="source">Objeto que se desea validar y que contiene marcas de <see cref="ValidationAttribute"/>.</param>
+        /// <param name="validationResults">Lista para almacenar los resultados de validación.</param>
+        /// <returns>true si el objeto es válido; de lo contrario, false.</returns>
+        public static bool TryValidateObject<TSource>(this TSource source, List<ValidationResult> validationResults)
+        {
+            var context = new ValidationContext(source);
+            return Validator.TryValidateObject(source, context, validationResults, true);
+        }
+        /// <summary>
+        /// Valida una propiedad específica de un objeto usando attributes de validación.
+        /// </summary>
+        /// <typeparam name="TSource">Tipo del objeto a validar.</typeparam>
+        /// <typeparam name="TProperty">Tipo de la propiedad a validar.</typeparam>
+        /// <param name="source">Objeto que contiene la propiedad a validar.</param>
+        /// <param name="propertyExpression">Expresión que representa la propiedad a validar.</param>
+        /// <param name="validationResults">Lista para almacenar los resultados de validación.</param>
+        /// <returns>true si la propiedad es válida; de lo contrario, false.</returns>
+        public static bool ValidateProperty<TSource, TProperty>(this TSource source,
+            Expression<Func<TSource, TProperty>> propertyExpression, List<ValidationResult> validationResults)
+        {
+            var propertyName = propertyExpression.GetFullPathProperty();
+            return source.ValidateProperty<TSource, TProperty>(propertyName, validationResults);
+        }
+        /// <summary>
+        /// Valida una propiedad específica de un objeto usando attributes de validación.
+        /// </summary>
+        /// <typeparam name="TSource">Tipo del objeto a validar.</typeparam>
+        /// <typeparam name="TProperty">Tipo de la propiedad a validar.</typeparam>
+        /// <param name="source">Objeto que contiene la propiedad a validar.</param>
+        /// <param name="propertyName">Nombre de la propiedad a validar.</param>
+        /// <param name="validationResults">Lista para almacenar los resultados de validación.</param>
+        /// <returns>true si la propiedad es válida; de lo contrario, false.</returns>
+        public static bool ValidateProperty<TSource, TProperty>(this TSource source,
+            string propertyName, List<ValidationResult> validationResults)
+        {
+            var context = new ValidationContext(source)
+            {
+                MemberName = propertyName
+            };
+            var value = source.GetPropertyValueOfPath(propertyName);
+            return Validator.TryValidateProperty(value, context, validationResults);
+        }
+    }
+}
---
+++ KUtilitiesCore/Extensions/TimeSpanExt.cs
@@ -0,0 +1,55 @@
+﻿using KUtilitiesCore.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KUtilitiesCore.Extensions
+{
+    public static class TimeSpanExt
+    {
+        /// <summary>
+        /// Convierte un objeto <see cref="TimeSpan"/> en un formato legible por humanos.
+        /// </summary>
+        /// <param name="ts">El <see cref="TimeSpan"/> que se desea formatear.</param>
+        /// <returns>
+        /// Una cadena que representa el <see cref="TimeSpan"/> en un formato legible, como milisegundos, segundos, minutos, horas o días,
+        /// dependiendo de la duración del intervalo de tiempo.
+        /// </returns>
+        public static string ToReadleFormat(this TimeSpan ts)
+        {
+            if (ts.TotalMilliseconds < 1000)
+            {
+                return $"{ts.Milliseconds} ms";
+            }
+            else
+            {
+                // formats and its cutoffs based on totalseconds
+                var cutoff = new SortedList<long, string>
+                    {
+                        {59, "{3:S}" },
+                        {60, "{2:M}" },
+                        {60*60-1, "{2:M}, {3:S}"},
+                        {60*60, "{1:H}"},
+                        {24*60*60-1, "{1:H}, {2:M}"},
+                        {24*60*60, "{0:D}"},
+                        {Int64.MaxValue , "{0:D}, {1:H}"}
+                    };
+                // find nearest best match
+                var find = cutoff.Keys.ToList()
+                   .BinarySearch((long)ts.TotalSeconds);
+                // negative values indicate a nearest match
+                var near = find < 0 ? Math.Abs(find) - 1 : find;
+                // use custom formatter to get the string
+                return String.Format(
+                    new HMSFormatter(),
+                    cutoff[cutoff.Keys[near]],
+                    ts.Days,
+                    ts.Hours,
+                    ts.Minutes,
+                    ts.Seconds);
+            }
+        }
+    }
+}
