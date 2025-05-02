0b090beb86d1eba2e46d2b3212067c3d8fd12a2a
Mike <dark_mabg@hotmail.com>
2025-05-02 13:11:22 -0600

    IDbParameterCollection y su implementación

--- KUtilitiesCore.DataAccess/DAL/IDbParameterCollection.cs
+++ KUtilitiesCore.DataAccess/DAL/IDbParameterCollection.cs
@@ -1,55 +1,93 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.Common;
 using System.Linq;
 using System.Linq.Expressions;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace KUtilitiesCore.DataAccess.DAL
 {
+    /// <summary>
+    /// Colección de parámetros de base de datos con funcionalidad de solo lectura y manipulación avanzada.
+    /// </summary>
+    /// <typeparam name="TParameter">Tipo de parámetro que deriva de <see cref="DbParameter"/>.</typeparam>
     public interface IDbParameterCollection<TParameter> : IReadOnlyCollection<TParameter>
         where TParameter : DbParameter
     {
         /// <summary>
-        /// Da acceso a DbParameter por el nombre del parametro asignado
+        /// Acceso por nombre a los parámetros.
         /// </summary>
-        /// <param name="parameterName"></param>
-        /// <returns></returns>
         TParameter this[string parameterName] { get; }
-        IDbParameterCollection<TParameter> Add<TSource, TValue>(TSource sourceObj,
-        Expression<Func<TSource, TValue>> propertyExpression, 
-        ParameterDirection direction = ParameterDirection.Input);
+
         /// <summary>
-        /// Agrega un parametro dado el tipo de objeto y el valor inicial del parametro
+        /// Agrega un parámetro basado en una propiedad.
         /// </summary>
-        /// <typeparam name="TType">Tipo del valor del parametro</typeparam>
-        /// <param name="parameterName">Nombre del parametro</param>
-        /// <param name="value">Valor inicial del parametro</param>
-        /// <param name="size">Establece el tamaño maximo en bytes de la columna</param>
-        /// <param name="scale">Número de posiciones decimales hasta donde se resuelve DBParameter</param>
-        /// <param name="precision">Número máximi de digitos utilizados para representar la propiedad</param>
-        /// <param name="direction">Especifica del tipo de parametro dentro de una consulta</param>
-        /// <returns></returns>
-        IDbParameterCollection<TParameter> Add<TType>(
-            string parameterName,
-            TType value, int size, byte scale, byte precision,
+        void Add<TSource, TValue>(TSource sourceObj,
+            Expression<Func<TSource, TValue>> propertyExpression,
             ParameterDirection direction = ParameterDirection.Input);
-        IDbParameterCollection<TParameter> Add<TType>(
-           string parameterName,
-           TType value,
-           ParameterDirection direction = ParameterDirection.Input);
-        IDbParameterCollection<TParameter> Add(
-           string parameterName,
-           object value, DbType dbType, int size, byte scale, byte precision,
-           ParameterDirection direction = ParameterDirection.Input);
-        IDbParameterCollection<TParameter> Add<TType>(
-       string parameterName,
-       ParameterDirection direction = ParameterDirection.Input);
-        IDbParameterCollection<TParameter> Add(
-           string parameterName,
-           DbType dbType,
-           ParameterDirection direction = ParameterDirection.Input);
+
+        /// <summary>
+        /// Agrega un parámetro con valor inicial y configuraciones.
+        /// </summary>
+        void Add<TType>(string parameterName, TType value,
+            int size, byte scale, byte precision,
+            ParameterDirection direction = ParameterDirection.Input);
+
+        /// <summary>
+        /// Agrega un parámetro con valor inicial.
+        /// </summary>
+        void Add<TType>(string parameterName, TType value,
+            ParameterDirection direction = ParameterDirection.Input);
+
+        /// <summary>
+        /// Agrega un parámetro con configuraciones avanzadas.
+        /// </summary>
+        void Add(string parameterName, object value,
+            DbType dbType, int size, byte scale, byte precision,
+            ParameterDirection direction = ParameterDirection.Input);
+
+        /// <summary>
+        /// Agrega un parámetro sin valor inicial.
+        /// </summary>
+        void Add<TType>(string parameterName,
+            ParameterDirection direction = ParameterDirection.Input);
+
+        /// <summary>
+        /// Agrega un parámetro especificando solo el tipo de datos.
+        /// </summary>
+        void Add(string parameterName, DbType dbType,
+            ParameterDirection direction = ParameterDirection.Input);
+
+        /// <summary>
+        /// Elimina todos los parámetros.
+        /// </summary>
+        void Clear();
+
+        /// <summary>
+        /// Verifica existencia de un parámetro por nombre.
+        /// </summary>
+        bool Contains(string parameterName);
+
+        /// <summary>
+        /// Elimina un parámetro por nombre.
+        /// </summary>
+        bool Remove(string parameterName);
+
+        /// <summary>
+        /// Elimina un parámetro específico.
+        /// </summary>
+        bool Remove(TParameter param);
+        /// <summary>
+        /// Obtiene el valor del parametro.
+        /// </summary>
+        /// <typeparam name="TValue">Especifica el tipo de conversion del objeto</typeparam>
+        /// <param name="parameterName">Nombre del parametro</param>
+        /// <returns></returns>
+        TValue GetParamValue<TValue>(string parameterName);
     }
 }
