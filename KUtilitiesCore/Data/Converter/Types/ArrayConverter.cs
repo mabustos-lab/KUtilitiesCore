315dd843eb38848608a30f01849cc33f7f23e078
Mike <dark_mabg@hotmail.com>
2025-06-09 00:33:46 -0600

    Mejora de documentación y funcionalidad en convertidores
    Se han añadido comentarios XML en la clase `ArrayConverter<TTargetType>` para documentar su funcionalidad, incluyendo descripciones de métodos y propiedades. Además, se ha implementado un nuevo método `TryConvert(string value)` que permite convertir un valor de texto en un arreglo de tipo de destino utilizando el separador definido.
    
    También se ha eliminado una línea de comentario en `DateGreaterThanAttribute.cs`, lo que sugiere una limpieza del código.

--- KUtilitiesCore/Data/Converter/Types/ArrayConverter.cs
+++ KUtilitiesCore/Data/Converter/Types/ArrayConverter.cs
@@ -2,16 +2,27 @@
 
 namespace KUtilitiesCore.Data.Converter.Types
 {
+    /// <summary>
+    /// Convierte un arreglo de cadenas en un arreglo de un tipo específico utilizando un convertidor interno.
+    /// </summary>
+    /// <typeparam name="TTargetType">Tipo de los elementos del arreglo de destino.</typeparam>
     public class ArrayConverter<TTargetType> : IArrayTypeConverter<TTargetType[]>
     {
         #region Fields
 
+        /// <summary>
+        /// Convertidor interno utilizado para convertir cada elemento individual.
+        /// </summary>
         private readonly ITypeConverter<TTargetType> internalConverter;
 
         #endregion Fields
 
         #region Constructors
 
+        /// <summary>
+        /// Inicializa una nueva instancia de la clase <see cref="ArrayConverter{TTargetType}"/>.
+        /// </summary>
+        /// <param name="internalConverter">Convertidor utilizado para convertir cada elemento del arreglo.</param>
         public ArrayConverter(ITypeConverter<TTargetType> internalConverter)
         {
             this.internalConverter = internalConverter;
@@ -21,19 +32,36 @@ namespace KUtilitiesCore.Data.Converter.Types
 
         #region Properties
 
+        /// <summary>
+        /// Obtiene o establece el carácter separador utilizado para dividir la cadena de entrada.
+        /// </summary>
         public char Separator { get; set; } = ',';
+
+        /// <summary>
+        /// Obtiene el tipo de destino al que se convierte la cadena.
+        /// </summary>
         public Type TargetType => typeof(TTargetType[]);
 
         #endregion Properties
 
         #region Methods
 
+        /// <summary>
+        /// Determina si el convertidor puede convertir el valor de texto especificado al tipo de destino.
+        /// </summary>
+        /// <param name="value">El valor de texto a evaluar.</param>
+        /// <returns>True si el convertidor puede convertir el valor; de lo contrario, false.</returns>
         public virtual bool CanConvert(string value)
         {
             string[] values = value.Split(Separator);
             return TryConvert(values, out _);
         }
 
+        /// <summary>
+        /// Intenta convertir un arreglo de cadenas al tipo de colección de destino.
+        /// </summary>
+        /// <param name="values">Arreglo de cadenas a convertir.</param>
+        /// <returns>El objeto convertido si la conversión es exitosa; de lo contrario, null.</returns>
         public virtual object? TryConvert(string[] values)
         {
             if (TryConvert(values, out TTargetType[] result))
@@ -43,6 +71,12 @@ namespace KUtilitiesCore.Data.Converter.Types
             return null;
         }
 
+        /// <summary>
+        /// Intenta convertir un arreglo de cadenas al arreglo de tipo de destino.
+        /// </summary>
+        /// <param name="value">Arreglo de cadenas a convertir.</param>
+        /// <param name="result">Resultado de la conversión si es exitosa; valor por defecto en caso contrario.</param>
+        /// <returns>True si la conversión fue exitosa; de lo contrario, false.</returns>
         public virtual bool TryConvert(string[] value, out TTargetType[] result)
         {
             result = new TTargetType[value.Length];
@@ -58,6 +92,11 @@ namespace KUtilitiesCore.Data.Converter.Types
             return true;
         }
 
+        /// <summary>
+        /// Intenta convertir el valor de texto especificado al tipo de destino.
+        /// </summary>
+        /// <param name="value">El valor de texto a convertir.</param>
+        /// <returns>El objeto convertido si la conversión es exitosa; de lo contrario, null.</returns>
         public virtual object? TryConvert(string value)
         {
             return TryConvert(value.Split(Separator));
