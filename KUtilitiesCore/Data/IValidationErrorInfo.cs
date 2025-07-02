582c030e1c2440ea619a3ce342bc401d27399613
Mike <dark_mabg@hotmail.com>
2025-07-02 16:04:15 -0600

    Añadir validación de objetos y propiedades
    Se han implementado nuevas clases e interfaces en el espacio de nombres `KUtilitiesCore.Data` para gestionar la validación de objetos. Se introdujo la interfaz `IValidationErrorInfo`, que extiende `IDataErrorInfo`, y se creó la clase abstracta `ValidationErrorInfoBase` para manejar mensajes de error. Se han añadido comentarios en español para mejorar la documentación y se incluyeron las directivas `using` necesarias.

---
+++ KUtilitiesCore/Data/IValidationErrorInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KUtilitiesCore.Data
+{
+    public interface IValidationErrorInfo:IDataErrorInfo
+    {
+        /// <summary>
+        /// Indica si hay algún mensaje de error en el objeto
+        /// </summary>
+        [NotMapped]
+        [Display(AutoGenerateField = false)]
+        bool HasValidationErrors { get; set; }
+        /// <summary>
+        /// Agrega una mensaje de error en una propiedad
+        /// </summary>
+        /// <param name="ErrMessage"></param>
+        /// <param name="ColumnName"></param>
+        void AddError(string ErrMessage, string ColumnName);
+        /// <summary>
+        /// Borra todos los mensajes de error de cada propiedad
+        /// </summary>
+        void ClearErrorInfo();
+        /// <summary>
+        /// Establece un mensaje de error del objeto
+        /// </summary>
+        /// <param name="errMessage"></param>
+        void SetError(string errMessage);
+    }
+}
