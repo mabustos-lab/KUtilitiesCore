9a3b1d66829910c685057d5261969ca3a441d197
Mike <dark_mabg@hotmail.com>
2025-07-02 01:29:09 -0600

    Refactorización de ChangeTrackingBase e implementación de IChangeTrackingBase
    Se han añadido las directivas `using` para `System.ComponentModel.DataAnnotations` y `System.ComponentModel.DataAnnotations.Schema` en `ChangeTrackingBase.cs`.
    
    La clase `ChangeTrackingBase` ahora implementa la interfaz `IChangeTrackingBase`, mejorando la gestión de cambios. Se ha añadido la propiedad `IsChanged` con atributos de datos para su mapeo y visualización.
    
    Se ha reestructurado el método `RestoreSnapshot` para mejorar la claridad, y se ha mantenido la lógica de `CurrentSnapshot` con nuevos atributos.
    
    Además, se han añadido comentarios y se ha reorganizado la sección de clases internas para mejorar la legibilidad y la estructura del código.

---
+++ KUtilitiesCore/Tracking/IChangeTrackingBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+
+namespace KUtilitiesCore.Tracking
+{
+    /// <summary>
+    /// Interfaz que implementa IEditableObject para gestionar el seguimiento y reversión de cambios.
+    /// </summary>
+    public interface IChangeTrackingBase:IEditableObject
+    {
+        /// <summary>
+        /// Indica si se han modificado los valores de las propiedades después de invocar BeginEdit.
+        /// </summary>
+        [NotMapped]
+        [Display(AutoGenerateField = false)]
+        bool IsChanged { get; }
+    }
+}
