e289f3d6b52bdd50df70a55fceb0b0453864e2d9
Mike <79868931+mabustos-lab@users.noreply.github.com>
2026-01-06 20:59:14 -0600

    Ordenar data importer

--- KUtilitiesCore.Data/DataExporter/EmptyDataSourceException.cs
+++ KUtilitiesCore.Data/DataExporter/EmptyDataSourceException.cs
@@ -5,7 +5,7 @@ using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace KUtilitiesCore.Data
+namespace KUtilitiesCore.Data.DataExporter
 {
     [Serializable]
     public class EmptyDataSourceException : Exception
--- KUtilitiesCore.Data/DataExporter/ExportToExcel.cs
+++ KUtilitiesCore.Data/DataExporter/ExportToExcel.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
-namespace KUtilitiesCore.Data
+namespace KUtilitiesCore.Data.DataExporter
 {
     /// <summary>
     /// Exporta un DataTable a un archivo Excel (.xlsx).
